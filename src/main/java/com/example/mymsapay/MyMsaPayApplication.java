2f2e30c2567a4e539adfd5c95d09a4159e607b30
zoomin3022 <zoomin3022@gmail.com>
2024-01-13 15:36:35 +0900

    Initial commit

---
+++ src/main/java/com/example/mymsapay/MyMsaPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymsapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMsaPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMsaPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mymsapay/MyMsaPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymsapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMsaPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
