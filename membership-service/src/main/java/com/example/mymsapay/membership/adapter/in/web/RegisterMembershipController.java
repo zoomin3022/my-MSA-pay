b357dc6e24c0565829b1a4f75e28236bb6934790
zoomin3022 <zoomin3022@gmail.com>
2024-01-16 21:51:43 +0900

    refactor: TestController -> RegisterMembershipController rename

--- membership-service/src/main/java/com/example/mymsapay/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/example/mymsapay/membership/adapter/in/web/RegisterMembershipController.java
@@ -1,11 +1,13 @@
 package com.example.mymsapay.membership.adapter.in.web;
 
+import com.example.mymsapay.WebAdapter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+@WebAdapter
+public class RegisterMembershipController {
 
     @RequestMapping("/test")
     public ResponseEntity<String> test() {
